ab16556cd32197cbcd9be77e0b226c8f0dcf1fd2
theartreporter <carl@theartreporter.com>
2026-03-25 11:00:01 -0700

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,11 @@
+'use client'
+
+import ChatInterface from '@/components/ChatInterface'
+
+export default function Home() {
+  return (
+    <main className="war-room min-h-screen">
+      <ChatInterface />
+    </main>
+  )
+}
